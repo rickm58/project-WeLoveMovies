a80297f3c23a17d8d7a32471d9a0a4b91fdb3c55
Rick Madison <rick.madison.58@gmail.com>
2022-11-09 14:35:54 -0500

    updates

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require("cors");
 
 app.use(cors())
-
+app.use(express.json())
 const notFound = require("./errors/notFound")
 const errorHandler = require("./errors/errorHandler")
 
@@ -13,7 +13,13 @@ const moviesRouter = require("./movies/movies.router")
 const theatersRouter = require("./theaters/theaters.router")
 const reviewsRouter = require("./reviews/reviews.router")
 
-app.use(express.json())
+const router = express.Router()
+router.get('/', cors(), (req, res) => {
+    res.json({ message:
+    'Welcome! You can access the data using these routes: /movies, /reviews, /theaters, /reviews/:reviewId, /movies/:movieId/theaters, and /movies/:movieId/reviews.'});
+})
+
+app.use('/', router)
 
 app.use("/movies", moviesRouter)
 app.use("/theaters", theatersRouter)
